7953b94b8dc0e32f3c1f3a8228f614aab0f56c49
Vahid <khodabakhshi.vahid@gmail.com>
2022-09-17 21:32:49 +0000

    Add calculating cluster using ML (not finished)

--- schindler/api/users.py
+++ schindler/api/users.py
@@ -2,7 +2,7 @@ import random
 
 import names
 
-from schindler.models import MAX_JOURNEY_FREQ, UserProfile, interests
+from schindler.models import MAX_JOURNEY_FREQ, Cluster, UserProfile, interests
 
 
 def generate_random_users(number):
@@ -19,8 +19,11 @@ def generate_random_users(number):
 def generate_random_user():
     user_interests = ','.join([interest for interest, _ in random.sample(interests, random.randint(1, 7))])
     user = UserProfile(name=names.get_full_name(), journey_frequency=random.randint(0, MAX_JOURNEY_FREQ),
-                       interests=user_interests)
+                       interests=user_interests, Cluster=calculate_cluster( user_interests ))
     user.interests = user_interests
 
     user.save()
     return user.to_json()
+
+def calculate_cluster( list_of_interests ):
+    pass
