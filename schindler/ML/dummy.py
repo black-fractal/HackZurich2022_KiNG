8638d985042b72bbe92594f05f31d102aa98a3ba
Reem Abdel-Salam <reem.abdelsalam13@gmail.com>
2022-09-18 01:00:45 +0200

    try fixing problem

--- schindler/ML/dummy.py
+++ schindler/ML/dummy.py
@@ -1,4 +1,23 @@
-from inference import tokenize_only,predict_cluster
+from inference import predict_cluster
+import nltk
+import re
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+import re
+from nltk.stem import WordNetLemmatizer 
+
+
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # print(tokens)
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    # print(filtered_tokens)
+    return filtered_tokens
 
 data_point={'name': 'Michael Delker',
  'interests': 'movie,adventure,science,politics,fashion,culture',
--- schindler/api/users.py
+++ schindler/api/users.py
@@ -1,11 +1,27 @@
 import random
 
 import names
-
+import nltk
+import re
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+import re
+from nltk.stem import WordNetLemmatizer 
 from schindler.ML.inference import *
 from schindler.models import MAX_JOURNEY_FREQ, Cluster, UserProfile, interests
 
 
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # print(tokens)
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    # print(filtered_tokens)
+    return filtered_tokens
 
 def generate_random_users(number):
     users = []
@@ -26,6 +42,10 @@ def generate_random_user():
 
     user.save()
     return user.to_json()
+
+
+
+
 
 def calculate_cluster( list_of_interests ):
     return predict_cluster( [list_of_interests] )
