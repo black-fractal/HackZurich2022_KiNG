97c6c375687566bee58c12ca7db871ae13d6a870
Z1gZ0g <master@z1gz0g.com>
2022-09-17 08:29:22 +0430

    fix redirect

--- schindler/views.py
+++ schindler/views.py
@@ -1,16 +1,18 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from schindler.api.lift import lifts_info
 
 
 def dashboard(request):
-    pass
+    return redirect('schindler-lifts')
 
 
 def lifts_view(request):
     return render(request, 'schindler/lifts.html')
 
 
+@csrf_exempt
 def lifts_api(request):
     return JsonResponse(lifts_info(), safe=False)
