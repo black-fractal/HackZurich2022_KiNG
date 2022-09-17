e1a550fe52c0cfd55cd9861ff08bc783c4728960
Taheri <a.taheri@shaparak.com>
2022-09-17 16:51:42 +0430

    refactor code

--- schindler/api/users.py
+++ schindler/api/users.py
@@ -5,13 +5,15 @@ import names
 from schindler.models import MAX_JOURNEY_FREQ, UserProfile, interests
 
 
-def generate_random_users( number ):
-    users = []        
-    for item in range( number ):
-        users.append( generate_random_user() )
+def generate_random_users(number):
+    users = list()
+    for item in range(number):
+        users.append(generate_random_user())
     return users
 
+
 def generate_random_user():
-    user = UserProfile( name=names.get_full_name(), interests=random.choice( interests )[0], journey_frequency=random.randint(0, MAX_JOURNEY_FREQ) )
+    user = UserProfile(name=names.get_full_name(), interests=random.choice(interests)[0],
+                       journey_frequency=random.randint(0, MAX_JOURNEY_FREQ))
     user.save()
     return user
