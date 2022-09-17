2c7e0b97fc321faf246531798269db0eda7d58aa
Z1gZ0g <master@z1gz0g.com>
2022-09-17 08:08:19 +0430

    add schindler urls to patterns

--- HackZurich2022_KiNG/urls.py
+++ HackZurich2022_KiNG/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
+from HackZurich2022_KiNG.settings import STATIC_ROOT, STATIC_URL
+from schindler.urls import urlpatterns as schindler_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
+urlpatterns += schindler_urlpatterns
